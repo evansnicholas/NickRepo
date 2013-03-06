77d9e22e41b0990be26578947b1cfb2b6e1187f4
Nick Evans <evansnicholas@hotmail.com>
2013-03-06 10:50:26 +0100

    Mid morning
    Linked excel file to XML generator.

--- ConfigFileGenerator/ExcelAPI/src/Main.java
+++ ConfigFileGenerator/ExcelAPI/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -18,24 +19,69 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		XMLFileCreator xmlFileCreator = new XMLFileCreator();
+		
 		try{
-		OPCPackage pkg = OPCPackage.open(new File("C:\\Users\\iggo\\Documents\\ExelAPITest.xlsx"));
+		OPCPackage pkg = OPCPackage.open(new File("C:\\Users\\iggo\\Desktop\\ConfigFileGeneratorMaterial\\SeverityConfigurationFile.xlsx"));
 		XSSFWorkbook wb = new XSSFWorkbook(pkg);
 		
-		Sheet sheet1 = wb.getSheetAt(0);
-		Row firstRow = sheet1.getRow(0);
-		Cell firstCell = firstRow.getCell(0);
-		String cellContent = firstCell.getRichStringCellValue().getString();
-		System.out.println(cellContent);
+		Sheet sheet1 = wb.getSheetAt(1);
 		
+		
+		for (Row row : sheet1){ 
+		
+			ArrayList<String> rowContent = new ArrayList<String>();
+			//Row firstRow = sheet1.getRow(1);
+		
+		//Cell testCell = firstRow.getCell(6);
+		//System.out.println(testCell.getCellType());
+		
+			for (Cell cell : row){
+			
+				int cellType = cell.getCellType();
+			
+				if (cellType == 1){
+					String cellContent = cell.getRichStringCellValue().getString();
+					rowContent.add(cellContent);
+					//System.out.println(cellContent);
+				}
+				else if (cellType == 4){
+					Boolean cellValueBoolean = (Boolean) cell.getBooleanCellValue();
+					String cellValueBooleanInString = cellValueBoolean.toString();
+					rowContent.add(cellValueBooleanInString);
+					//System.out.println(cellValueBooleanInString);	
+				}
+				else if (cellType == 0){
+					int cellValueInt = (int) cell.getNumericCellValue();
+					Integer cellValueIntInInteger = (Integer) cellValueInt;
+					String cellValueIntegerInString = cellValueIntInInteger.toString();
+					rowContent.add(cellValueIntegerInString);
+					//System.out.println(cellValueIntegerInString);
+				}
+				else if (cellType == 3){
+					
+					rowContent.add("REPEATED_SERVICE");
+					
+				}
+			}
+			
+			xmlFileCreator.addRow(rowContent);
+			
+			
+		}
+			
 		pkg.close();
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
 		
-		XMLFileCreator xmlFileCreator = new XMLFileCreator();
+		//remove last 4 entries because we don't want them.
+		
+		xmlFileCreator.removeLast4ElementsFromRowsAndFirstRow();
+		xmlFileCreator.printSizeOfRows();
 		xmlFileCreator.makeXMLFile();
+		
 		
 		
 		
--- ConfigFileGenerator/ExcelAPI/src/XMLFileCreator.java
+++ ConfigFileGenerator/ExcelAPI/src/XMLFileCreator.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,59 +16,111 @@ import org.w3c.dom.Element;
 
 public class XMLFileCreator {
 
-	
+	ArrayList<ArrayList> rows = new ArrayList<ArrayList>();
 	
 	public void makeXMLFile(){
 		try{
-		
+			
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			docFactory.setNamespaceAware(true);
 			
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 			Document doc = docBuilder.newDocument();
+			ServiceConfigurations sc = new ServiceConfigurations(doc);
 			
-		    ServiceConfigurations sc = new ServiceConfigurations(doc);
-			
-			
-			ServiceConfiguration serviceConfiguration = new ServiceConfiguration("as.EBSODS.Customer", "1", doc, "true");
-			sc.addServiceConfiguration(serviceConfiguration);
-			sc.addChildrenToRootElement();
-			
-			Logging logging = new Logging(doc, "local");
-			serviceConfiguration.addLogging(logging);
-			
-			Logging logging2 = new Logging(doc, "clever");
-			serviceConfiguration.addLogging(logging2);
-			
-			serviceConfiguration.addLoggingsToServiceConfiguration();
-			
-			Filter filter1 = new Filter(doc, "2", "INFO", "true");
-			logging.addFilter(filter1);
-			
-			Filter filter2 = new Filter(doc, "3", "DEBUG", "true");
-			logging.addFilter(filter2);
-			
-			logging.addFiltersToLogging();
-			
-			Filter filter3 = new Filter(doc, "2", "INFO", "true");
-			logging2.addFilter(filter3);
-			
-			Filter filter4 = new Filter(doc, "3", "DEBUG", "true");
-			logging2.addFilter(filter4);
-			
-			logging2.addFiltersToLogging();
-			
+			for (ArrayList row : rows){
+				
+				System.out.println("Number of elements in row: "+row.size());
+				
+				String serviceName = row.get(1).toString();
+				
+				String service = row.get(1).toString();
+				String isInternalHeader = row.get(2).toString();
+				String version = row.get(3).toString();
+				String target = row.get(4).toString();
+				String Severity_INFO = row.get(5).toString();
+				String INFO_Details = row.get(6).toString();
+				String Severity_DEBUG = row.get(7).toString();
+				String DEBUG_Details = row.get(8).toString();
+				String Severity_ERROR = row.get(9).toString();
+				String ERROR_Details = row.get(10).toString();
+				String Severity_FATAL = row.get(11).toString();
+				String FATAL_Details = row.get(12).toString();
+				String Severity_TRACE = row.get(13).toString();
+				String TRACE_Details = row.get(14).toString();
+				String Severity_WARNING = row.get(15).toString();
+				String WARNING_Details = row.get(16).toString();
+				
+				ServiceConfiguration serviceConfiguration = new ServiceConfiguration(service, version.toString(), doc, isInternalHeader);
+				sc.addServiceConfiguration(serviceConfiguration);
+				sc.addChildrenToRootElement();
+				
+				Logging logging = new Logging(doc, target);
+				serviceConfiguration.addLogging(logging);
+				
+				serviceConfiguration.addLoggingsToServiceConfiguration();
+				
+				Filter filterINFO = new Filter(doc, INFO_Details, "INFO", Severity_INFO);
+				logging.addFilter(filterINFO);
+				
+				Filter filterDEBUG = new Filter(doc, DEBUG_Details, "DEBUG", Severity_DEBUG);
+				logging.addFilter(filterDEBUG);
+				
+				Filter filterFATAL = new Filter(doc, FATAL_Details, "FATAL", Severity_FATAL);
+				logging.addFilter(filterFATAL);
+				
+				Filter filterERROR = new Filter(doc, ERROR_Details, "ERROR", Severity_ERROR);
+				logging.addFilter(filterERROR);
+				
+				Filter filterWARNING = new Filter(doc, WARNING_Details, "WARNING", Severity_WARNING);
+				logging.addFilter(filterWARNING);
+				
+				Filter filterTRACE = new Filter(doc,TRACE_Details, "TRACE", Severity_TRACE);
+				logging.addFilter(filterTRACE);
+				
+				logging.addFiltersToLogging();
+				
+			}	
+				
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(new File("C:\\Users\\iggo\\Desktop\\TestConfigFile.xml"));
-			
+				
 			transformer.transform(source, result);
 		
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+	
+	public void addRow(ArrayList rowContent){
+		
+		rows.add(rowContent);
+		
+	}
+	
+	public void printSizeOfRows(){
+		
+		System.out.println("Number of rows: "+rows.size());
+		
+	}
+	
+	public void removeLast4ElementsFromRowsAndFirstRow(){
+		
+		int rowsInitialSize = rows.size();
+		
+		for (int i=1; i<5; i++){
+			rows.remove(rowsInitialSize-i);
+		}
+		
+		rows.remove(0);
+	}
+	
+	public ArrayList getRows(){
+		return rows;
+	}
+	
 	
 	
 }
