227818aa75364ecbe853ea5597a79f197ffc8ba7
Nick Evans <evansnicholas@hotmail.com>
2013-04-18 12:53:02 +0200

    Added xsdHandler

--- BWCodeGeneration/BWCodeGeneration/src/nl/ziggo/icc/tooling/codegenerator/CodeGenerationManager.java
+++ BWCodeGeneration/BWCodeGeneration/src/nl/ziggo/icc/tooling/codegenerator/CodeGenerationManager.java
@@ -2,8 +2,11 @@ package nl.ziggo.icc.tooling.codegenerator;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JTextArea;
+
+import nl.ziggo.icc.tooling.codegenerator.entity.Service;
 
 import org.apache.commons.io.FileUtils;
 
@@ -12,130 +15,139 @@ public class CodeGenerationManager {
 
 	String componentName;
 	JTextArea log;
-	ArrayList<String[]> termsToBeReplaced;
+	HashMap<String, String> placeHolders;
 	
-	public CodeGenerationManager(String componentName, JTextArea log, ArrayList<String[]> termsToBeReplaced){
+	public CodeGenerationManager(String componentName, JTextArea log, HashMap<String, String> placeHolders){
 	
 		this.componentName = componentName;
 		this.log = log;
-		this.termsToBeReplaced = termsToBeReplaced;
+		this.placeHolders = placeHolders;
 		
 	}
 	
 	public boolean generateBWCodeForComponent(){
 		
-		boolean isSuccessful;
+		CodeGenerationXsdHandler xsdHandler = new CodeGenerationXsdHandler();
 		
+		boolean isSuccessful;
+		Service[] services = xsdHandler.getServicesFromXsd(componentName);
 		File[] templates = CodeGeneratorConfiguration.templatesLocation.listFiles();
 		
-		for (File template : templates){
-			
-			boolean isFolderName = false;
-			
-			if (template.isDirectory() == true){
+			for (Service service : services){
+		
+				HashMap<String, String> serviceInformation = service.getServiceInformation();
 				
-				continue;
-				
-			}else{	
-				
-				
-				try{
-				
-					//Get template as string
-					String fileContents = FileUtils.readFileToString(template);
+				for (File template : templates){
 					
-					//Replace all necessary values
-					for (String[] termPair : this.termsToBeReplaced){
+					boolean isFolderName = false;
 					
-						fileContents = fileContents.replaceAll(termPair[0], termPair[1]);
-					
-					}
-					
-					//Performing parsing of template Name
-					String partialDestinationPath = "";
-					String templateName = template.getName();
-					
-					if (templateName.endsWith(".directory")){
+					if (template.isDirectory() == true){
 						
-						isFolderName = true;
+						continue;
 						
-						int lastDotIndex = templateName.lastIndexOf(".");
+					}else{	
 						
-						templateName = templateName.substring(0, lastDotIndex);
 						
-					}
-
-					String[] templateNameParts = templateName.split("-");
-					
-					
-					
-					for (String part : templateNameParts){
+						try{
 						
-						for (String[] termPair : termsToBeReplaced){
+							//Get template as string
+							String fileContents = FileUtils.readFileToString(template);
 							
-							part = part.replaceAll(termPair[0], termPair[1]);
+							//Replace all necessary values
+							for (String key : placeHolders.keySet()){
+							
+								fileContents = fileContents.replaceAll(placeHolders.get(key), serviceInformation.get(key));
+							
+							}
+							
+							//Performing parsing of template Name
+							String partialDestinationPath = "";
+							String templateName = template.getName();
+							
+							if (templateName.endsWith(".directory")){
+								
+								isFolderName = true;
+								
+								int lastDotIndex = templateName.lastIndexOf(".");
+								
+								templateName = templateName.substring(0, lastDotIndex);
+								
+							}
+		
+							String[] templateNameParts = templateName.split("-");
+							
+							
+							
+							for (String part : templateNameParts){
+								
+								for (String key : placeHolders.keySet()){
+									
+									part = part.replaceAll(placeHolders.get(key), serviceInformation.get(key));
+									
+								}
+								
+								partialDestinationPath = partialDestinationPath+"\\"+part;
+								
+							}	
+							
+							//Create the final destination path for the code
+							File finalGeneratedFile = new File(CodeGeneratorConfiguration.svnComponentsFile.getPath()+"\\"+this.componentName+"\\trunk\\src\\"+this.componentName+"\\"+partialDestinationPath);
+							
+							//Make the necessary directories for the file if file is not a directory
+							
+							if (isFolderName == false){
+								
+								finalGeneratedFile.getParentFile().mkdirs();
+								
+							}else{
+								
+								finalGeneratedFile.mkdirs();
+								
+							}
+							
+							
+							//Write the string to a file in the correct SVN location.
+							if (isFolderName == false){
+								
+								FileUtils.writeStringToFile(finalGeneratedFile, fileContents);
+							
+							}
+							
+							//Log what has been done
+							log.append("Created: " + finalGeneratedFile.getAbsolutePath() + "\n");
+						    log.setCaretPosition(log.getDocument().getLength());
+							
+							
+						}catch(IOException e ){
+							
+							try{
+								
+								FileUtils.cleanDirectory(new File(CodeGeneratorConfiguration.svnComponentsFile.getPath()+"\\"+this.componentName+"\\trunk\\src\\"+this.componentName));
+								
+							}catch(IOException e2){
+								
+								log.append("An error occurred: " + e2.getMessage() + "  Code generation aborted.\n");
+							    log.setCaretPosition(log.getDocument().getLength());
+							    return isSuccessful = false;
+								
+								
+							}
+							
+							log.append("An error occurred: " + e.getMessage() + "  Code generation aborted.\n");
+						    log.setCaretPosition(log.getDocument().getLength());
+						  
+						    return isSuccessful = false;
 							
 						}
-						
-						partialDestinationPath = partialDestinationPath+"\\"+part;
-						
-					}	
 					
-					//Create the final destination path for the code
-					File finalGeneratedFile = new File(CodeGeneratorConfiguration.svnComponentsFile.getPath()+"\\"+this.componentName+"\\trunk\\src\\"+this.componentName+"\\"+partialDestinationPath);
-					
-					//Make the necessary directories for the file if file is not a directory
-					
-					if (isFolderName == false){
-						
-						finalGeneratedFile.getParentFile().mkdirs();
-						
-					}else{
-						
-						finalGeneratedFile.mkdirs();
-						
-					}
-					
-					
-					//Write the string to a file in the correct SVN location.
-					if (isFolderName == false){
-						
-						FileUtils.writeStringToFile(finalGeneratedFile, fileContents);
-					
-					}
-					
-					//Log what has been done
-					log.append("Created: " + finalGeneratedFile.getAbsolutePath() + "\n");
-				    log.setCaretPosition(log.getDocument().getLength());
-					
-					
-				}catch(IOException e ){
-					
-					try{
-						
-						FileUtils.cleanDirectory(new File(CodeGeneratorConfiguration.svnComponentsFile.getPath()+"\\"+this.componentName+"\\trunk\\src\\"+this.componentName));
-						
-					}catch(IOException e2){
-						
-						log.append("An error occurred: " + e2.getMessage() + "  Code generation aborted.\n");
-					    log.setCaretPosition(log.getDocument().getLength());
-					    return isSuccessful = false;
-						
-						
-					}
-					
-					log.append("An error occurred: " + e.getMessage() + "  Code generation aborted.\n");
-				    log.setCaretPosition(log.getDocument().getLength());
-				  
-				    return isSuccessful = false;
-					
+					}			
+				
 				}
-			
-			}			
-		
-		}
+				
+			}
 		
 		return isSuccessful = true;
 	}
+	
 	
 }
--- BWCodeGeneration/BWCodeGeneration/src/nl/ziggo/icc/tooling/codegenerator/entity/Service.java
+++ BWCodeGeneration/BWCodeGeneration/src/nl/ziggo/icc/tooling/codegenerator/entity/Service.java
@@ -1,15 +1,57 @@
 package nl.ziggo.icc.tooling.codegenerator.entity;
 
+import java.util.HashMap;
+
 public class Service {
 
 	private String adapterName;
 	private String operationName;
 	private String operationVersion;
 	private String cdmEntity;
+	private HashMap<String, String> serviceInformation;
 	
 	public Service(String adapterName){
 		
 		this.adapterName = adapterName;
+		
+	}
+	
+	public void setOperationName(String operationName){
+		this.operationName = operationName;	
+	}
+	
+	public void setOperationVersion(String operationVersion){
+		this.operationVersion = operationVersion;
+	}
+	
+	public void setCdmEntity(String cdmEntity){
+		this.cdmEntity = cdmEntity;
+	}
+	
+	public String getOperationName(){
+		return this.operationName;
+	}
+	
+	public String getOperationVersion(){
+		return this.operationVersion;
+	}
+	
+	public String getcdmEntity(){
+		return this.cdmEntity;
+	}
+	
+	public HashMap<String, String> getServiceInformation(){
+		
+		HashMap<String, String> serviceInformation = new HashMap<String, String>();
+		
+		serviceInformation.put("cdmEntity", this.cdmEntity);
+		serviceInformation.put("operationName", this.operationName);
+		serviceInformation.put("operationVersion", this.operationVersion);
+		serviceInformation.put("adapterName", this.adapterName);
+		serviceInformation.put("adapterNameLowerCase", this.adapterName.toLowerCase());
+		serviceInformation.put("operationNameLowerCase", this.operationName.toLowerCase());
+		
+		return serviceInformation;
 		
 	}
 	
