8e698edfa7f328548bcb72a1dc20c9f8fa0c8f86
Nick Evans <evansnicholas@hotmail.com>
2013-04-18 12:57:02 +0200

    XsdHandler

---
+++ BWCodeGeneration/BWCodeGeneration/src/nl/ziggo/icc/tooling/codegenerator/CodeGenerationXsdHandler.java
@@ -0,0 +1,94 @@
+package nl.ziggo.icc.tooling.codegenerator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.HashMap;
+
+import nl.ziggo.icc.tooling.codegenerator.entity.Service;
+
+public class CodeGenerationXsdHandler {
+
+	
+	
+	public Service[] getServicesFromXsd(String componentName){
+		
+		File xsdFileDirectory = new File(CodeGeneratorConfiguration.svnComponentsFile.toPath()+"\\"+componentName+"\\trunk\\resource\\Schema");
+		Service[] services;
+		File[] xsds;
+		
+		if (!xsdFileDirectory.exists()){
+			
+			return null;
+			
+		}else{
+			
+			xsds = xsdFileDirectory.listFiles();
+			int numberOfServices = xsds.length;
+			services = new Service[numberOfServices];
+			
+		}
+		
+		int serviceNumber = 0;
+		
+		for (File xsd : xsds){
+			
+			Service service = new Service(componentName);
+			
+			HashMap<String, String> serviceInformation = this.getServiceInformationFromXsd(xsd);
+			
+			service.setCdmEntity(serviceInformation.get("cdmEntity"));
+			service.setOperationName(serviceInformation.get("operationName"));
+			service.setOperationVersion(serviceInformation.get("operationVersion"));
+			
+			services[serviceNumber] = service;
+			
+			serviceNumber++;
+		}
+		
+		return services;
+	}
+	
+	
+	public HashMap<String, String> getServiceInformationFromXsd(File xsd){
+		
+		HashMap<String, String> serviceInformation = new HashMap<String, String>();
+		
+		try (BufferedReader reader = Files.newBufferedReader(xsd.toPath(), StandardCharsets.UTF_8)) {
+			
+		    String line = null;
+		    
+		    while ((line = reader.readLine()) != null) {
+		    	
+		    		if (!line.contains("targetNamespace=")){
+		    			continue;
+		    		}else{
+		    			
+		    			int indexOfStartOfServiceInfo = line.indexOf("targetNamespace")+62;
+		    			int indexOfEndOfServiceInfo = line.indexOf("\"", indexOfStartOfServiceInfo);
+		    			String namespace = line.substring(indexOfStartOfServiceInfo, indexOfEndOfServiceInfo);
+		    			String[] namespaceSplit = namespace.split("/");
+		    			
+		    			serviceInformation.put("cdmEntity", namespaceSplit[0]);
+		    			serviceInformation.put("operationName", namespaceSplit[1]);
+		    			serviceInformation.put("operationVersion", namespaceSplit[2]);    			
+		    			
+		    			return serviceInformation;
+		    		}
+		    	
+		    }
+		
+		
+		}catch(IOException e){
+			
+			return null;
+			
+		}
+	
+		return null;
+		
+	}
+	
+}
